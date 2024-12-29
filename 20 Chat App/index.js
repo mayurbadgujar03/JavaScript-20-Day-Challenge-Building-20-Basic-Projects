51a42cf74733db01b2ad939aa35d1434d005289e
Mayur Badgujar <136896698+mayurbadgujar03@users.noreply.github.com>
2024-12-29 23:36:52 +0530

    Update index.js

--- 20 Chat App/index.js
+++ 20 Chat App/index.js
@@ -7,13 +7,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Socket.io connection
 io.on('connection', (socket) => {
     console.log('A user connected');
     
-    // Handle user message
     socket.on('user-message', (message) => {
-        // Broadcast message to all clients except the sender
         socket.broadcast.emit("message", { text: message, timestamp: new Date().toLocaleTimeString() });
     });
 
@@ -22,15 +19,11 @@ io.on('connection', (socket) => {
     });
 });
 
-// Serve static files (like index.html)
 app.use(express.static(path.resolve('./public')));
-
-// Serve index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('./public/index.html'));
 });
 
-// Listen on port 9000
 server.listen(9000, () => {
     console.log("Server started at http://localhost:9000");
 });
